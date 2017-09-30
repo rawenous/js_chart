95433af4c88dcb928d5f4039bb638cecc81a11a9
Rickard Claesson <riccl924@gmail.com>
2017-09-30 11:16:41 +0200

    Perform multiple queries from influxdb

--- linechart.js
+++ linechart.js
@@ -1,10 +1,10 @@
-date1 = "2017-08-17T09:18:01Z";
-date2 = "2017-08-17T11:18:01Z";
+date1 = "2017-08-17T09:18:01.702900257Z";
+date2 = "2017-08-17T11:18:01.702900257Z";
 minDate = d3.isoParse(date1).getTime();
 maxDate = d3.isoParse(date2).getTime();
 
-// minDate = d3.isoParse("2015-01-05T21:55:43.702900257Z").getTime();
-// maxDate = d3.isoParse("2015-03-05T21:55:43.702900257Z").getTime();
+// minDate = d3.isoParse("2017-08-17T09:18:01.702900257Z").getTime();
+// maxDate = d3.isoParse("2017-08-17T11:18:01.702900257Z").getTime();
 
 DATABASE = "epgdb";
 
@@ -42,24 +42,18 @@ lineGen = d3.line()
 var lineColor = d3.scaleOrdinal(d3.schemeCategory10);
 
 function build_dashboard(dash_data) {
-
-    console.log("dash_data:" + dash_data);
     graphs = dash_data.graphs;
     var id = 0;
 
     graphs.forEach(function (d) {
         var graphId = "graph_" + id;
         addChart(d, graphId);
-        testReq(d, graphId, addChartData);
+        testReq(d, graphId, addChartData, 0);
         id++;
     });
 }
 
 function parseMeasurementData(graph_query) {
-    // return getMeasurement(graph_query).then(JSON.parse).then(function(response) {
-    //     console.log("yay json!" + response.results[0].series[0]);
-    // });
-
     return getMeasurement(graph_query).then(JSON.parse);
 }
 function getMeasurement(graph_query) {
@@ -68,14 +62,15 @@ function getMeasurement(graph_query) {
 
             var httpRequest = new XMLHttpRequest();
             var query = `select ${graph_query.field_key} from ${graph_query.measuerment} where time >= '${date1}' and time <= '${date2}'`;
-            console.log("db query: " + query);
-            var url = `http://localhost:8086/query?db=${DATABASE}&q=${query}`;
+            var max_query = `select max(\"${graph_query.field_key}\") from ${graph_query.measuerment} where time >= '${date1}' and time <= '${date2}'`;
+            var url = `http://localhost:8086/query?db=${DATABASE}&q=${query}%3B${max_query}`;
+            console.log("db query: " + url);
             httpRequest.open('GET', url);
 
             httpRequest.onload = function() {
                 if (httpRequest.status === 200) {
                     // Perfect!
-                    console.log(httpRequest.response);
+                    //console.log(httpRequest.response);
                     resolve(httpRequest.response);
                 } else {
                     // Otherwise reject with the status text
@@ -101,10 +96,8 @@ function testReq(graph, id, chartDataFunction, depth) {
 
     Promise.all(promises)
         .then(function(data) {
-            //data.forEach(function(measurement_data) {
             chartDataFunction(graph, id, data, depth);
             console.log("response:" + data);
-            //});
     }).catch(function(error) {
         console.log("db query failed: " + error);
     });
@@ -152,22 +145,23 @@ function addChartData(graphInfo, id, data_list, depth) {
     var maxValue = 0;
     var graphData = [];
 
+    // Go through each query and collect values and max value
     data_list.forEach(function(json_data) {
         var results = json_data.results;
         for(var i = 0; i < results.length; i++) {
             var curValues = results[i].series[0].values;
 
-            graphData.push({"name" : results[i].series[0].columns[1],
-                            "values" : curValues});
-
-            // Find largest value so we can scale the y axis
-            for(var j = 0; j < curValues.length; j++) {
-                var value = curValues[i][1];
-                if (maxValue < value) {
-                    maxValue = value;
+            // Max is always present for every query
+            if(results[i].series[0].columns[1] == "max") {
+                if (maxValue < curValues[0][1]) {
+                    maxValue = curValues[0][1];
                 }
-
             }
+            else {
+                graphData.push({"name" : results[i].series[0].columns[1],
+                                "values" : curValues});
+            }
+
         }
     });
 
@@ -188,11 +182,10 @@ function addChartData(graphInfo, id, data_list, depth) {
     for (var k = 0; k < graphData.length; k++) {
         // Add larger strokewidth to drilldown line
         var strokeWidth = 2;
-        if  (graphInfo.queries[k].drilldown != "") {
+        var drilldown_q  = graphInfo.queries[k].drilldown;
+        if  (drilldown_q != "") {
             strokeWidth = 3;
         }
-
-        console.log("woho curvalues" + graphData[k].values);
 
         vis.append("path")
             .attr("class", "line_" + k)
@@ -202,11 +195,11 @@ function addChartData(graphInfo, id, data_list, depth) {
             })
             .attr("stroke-width", strokeWidth)
             .attr("fill", "none")
-
+            .attr("data-legend", graphData[k].name)
             .on("click", function() {
-                if (graphInfo.queries[k].drilldown != "") {
+                if (drilldown_q != "") {
                     depth++;
-                    testReq(getChartByName(graphInfo.queries[k].drilldown), id, updateChartData, depth);
+                    testReq(getChartByName(drilldown_q), id, updateChartData, depth);
                 }
             });
     }
@@ -223,6 +216,13 @@ function addChartData(graphInfo, id, data_list, depth) {
         .attr("transform", "translate(" + (MARGINS.left) + ",0)")
         .call(yAxis);
 
+
+    vis.selectAll("[data-legend]").each(function() {
+        var self = d3.select(this);
+        console.log("data-legend2: " + self.attr("data-legend"));
+
+
+    });
 
         // TODO: I cant get append and select combined with the data/enter functions to work.
         // console.log("about to draw data! graphData.length=" + graphData.length);
@@ -284,49 +284,49 @@ function updateChartData(graphInfo, id, data_list, depth) {
         return;
     }
 
-        var maxValue = 0;
-        var graphData = [];
+    var maxValue = 0;
+    var graphData = [];
 
 
     data_list.forEach(function(json_data) {
-
         var results = json_data.results;
         for(var i = 0; i < results.length; i++) {
             var curValues = results[i].series[0].values;
-            graphData.push({"name" : results[i].series[0].name,
-                            "values" : curValues});
 
-            // Find largest value so we can scale the y axis
-            for(var j = 0; j < curValues.length; j++) {
-                var value = curValues[i][1];
-                if (maxValue < value) {
-                    maxValue = value;
+            // Max is always present for every query
+            if(results[i].series[0].columns[1] == "max") {
+                if (maxValue < curValues[0][1]) {
+                    maxValue = curValues[0][1];
                 }
-
             }
+            else {
+                graphData.push({"name" : results[i].series[0].columns[1],
+                                "values" : curValues});
+            }
+
         }
     });
 
 
-        // Scale the range of the data
-        xScale.domain([minDate, maxDate]);
-        yScale.domain([0, maxValue]);
+    // Scale the range of the data
+    xScale.domain([minDate, maxDate]);
+    yScale.domain([0, maxValue]);
 
-        // Save current chart name
-        currentChartName = d3.select("." + id).select("text")
-            .html();
+    // Save current chart name
+    currentChartName = d3.select("." + id).select("text")
+        .html();
 
-        // And override with new chart name
-        d3.select("." + id).select("text")
-            .text(graphInfo.name);
+    // And override with new chart name
+    d3.select("." + id).select("text")
+        .text(graphInfo.name);
 
-        // Select the section we want to apply our changes to
-        // TODO: Why didn't select by id work? Because you should've done like this
-        // d3.select("#" + id)
-        var svg = d3.select("." + id);
+    // Select the section we want to apply our changes to
+    // TODO: Why didn't select by id work? Because you should've done like this
+    // d3.select("#" + id)
+    var svg = d3.select("." + id);
 
-        // Add circle that goes back when you click it
-        if (depth != 0) {
+    // Add circle that goes back when you click it
+    if (depth != 0) {
         var svg_back = d3.select("." + id).append("circle")
             .attr("r", 10)
             .attr("cx", WIDTH - MARGINS.right)
@@ -337,41 +337,41 @@ function updateChartData(graphInfo, id, data_list, depth) {
                 testReq(getChartByName(currentChartName), id, updateChartData, depth);
                 d3.select(this).remove();
             });
-        }
-        // Add the valueline path.
-        for (var k = 0; k < graphData.length; k++) {
+    }
+    // Add the valueline path.
+    for (var k = 0; k < graphData.length; k++) {
 
-            // Add the drilldown functionality for this chart
-            svg.select(".line_" + k)
-                .on("click", function() {
-                    // Second item in queries is the subchart for this line
+        var drilldown_q = graphInfo.queries[k].drilldown;
+        // Add the drilldown functionality for this chart
+        svg.select(".line_" + k)
+            .on("click", function() {
+                // Second item in queries is the subchart for this line
+                if (drilldown_q != "") {
+                    depth++;
+                    testReq(getChartByName(drilldown_q), id, updateChartData, depth);
+                }
+            });
 
-                    if (graphInfo.queries[k].drilldown != "") {
-                        depth++;
-                        testReq(getChartByName(graphInfo.name), id, updateChartData, depth);
-                    }
-                });
+        svg.select(".line_" + k)
+            .transition()
+            .duration(750)
+            .attr("d", lineGen(graphData[k].values))
+            .attr("stroke", function() {
+                return lineColor(k);
+            })
+            .attr("stroke-width", 2)
+            .attr("fill", "none");
 
-            svg.select(".line_" + k)
-                .transition()
-                .duration(750)
-                .attr("d", lineGen(graphData[k].values))
-                .attr("stroke", function() {
-                    return lineColor(k);
-                })
-                .attr("stroke-width", 2)
-                .attr("fill", "none");
+        svg.select(".x_axis") // change the x axis
+            .transition()
+            .duration(750)
+            .call(xAxis);
+        svg.select(".y_axis") // change the y axis
+            .transition()
+            .duration(750)
+            .call(yAxis);
 
-            svg.select(".x_axis") // change the x axis
-                .transition()
-                .duration(750)
-                .call(xAxis);
-            svg.select(".y_axis") // change the y axis
-                .transition()
-                .duration(750)
-                .call(yAxis);
-
-        }
+    }
 }
 
 d3.json("dashboard.json", function(d) {
